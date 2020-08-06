be9fb5e4db45ad5ca37bea2d27f02f53ea91657f
Adriana Gomez <gomezhinojosa.adriana@gmail.com>
2020-08-06 18:55:25 -0500

    Updates

---
+++ precache-manifest.d75fee1ee56cefc50d252419144183ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e28d94d35fdbf8d154603cc4aa281e9c",
+    "url": "/Pokemon/index.html"
+  },
+  {
+    "revision": "6562994b8184107f9e96",
+    "url": "/Pokemon/static/css/main.ec5efbe5.chunk.css"
+  },
+  {
+    "revision": "b0dc24abd0621fd7cda2",
+    "url": "/Pokemon/static/js/2.7457f995.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Pokemon/static/js/2.7457f995.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6562994b8184107f9e96",
+    "url": "/Pokemon/static/js/main.8d69cdd8.chunk.js"
+  },
+  {
+    "revision": "88f10aed6ed0ea6669b1",
+    "url": "/Pokemon/static/js/runtime-main.74e75df6.js"
+  },
+  {
+    "revision": "a5b2d57acb70d3d021940e0828b92635",
+    "url": "/Pokemon/static/media/logo.a5b2d57a.png"
+  }
+]);
